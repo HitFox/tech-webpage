21ddcaf0858a8b700fa543f4c3c3f874f88bdc67
verpixelt <kevin.lorenz@hitfoxgroup.com>
2015-11-12 18:34:07 +0100

    Rewrite the scroll script

--- src/javascripts/scroll.js
+++ src/javascripts/scroll.js
@@ -1,21 +1,27 @@
-document.addEventListener("DOMContentLoaded", function(event) {
 
-  var container = document;
-  var imageClass = document.getElementsByClassName('img--is-in-background__img')[0];
-  var imageHeight = imageClass.clientHeight;
-  var documentHeader = document.getElementsByTagName('header')[0];
-  var documentHeaderHeight = documentHeader.clientHeight;
-  var documentHeaderDifference = imageHeight - documentHeaderHeight;
 
-  console.log(imageHeight);
-  console.log(documentHeaderDifference);
+(function() {
+  var container = document.body;
 
-  container.onscroll = function () {
-    var y = window.scrollY;
-    if(y > documentHeaderDifference) {
+  var checkScroll = function() {
+    var imageHeader = document.getElementsByClassName('img--is-in-background__img')[0];
+    var scrollPosition = container.scrollTop;
+    var documentHeader = document.getElementsByTagName('header')[0];
+    var imageHeight = imageHeader.clientHeight - documentHeader.clientHeight;
+
+    if(scrollPosition > imageHeight) {
+      console.log('yeeeehaaaa');
       documentHeader.classList.add('is-scrolled');
     } else {
       documentHeader.classList.remove('is-scrolled');
     }
   };
-});
+
+  window.addEventListener('scroll', function() {
+    checkScroll();
+  });
+
+  window.addEventListener('resize', function() {
+    checkScroll();
+  });  
+}) ();
